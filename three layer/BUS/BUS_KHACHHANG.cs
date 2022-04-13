246d25d8b495a122f60f52bff8b8bb21cf033c14
GHuy <92932665+NHGHuy@users.noreply.github.com>
2022-04-13 14:51:08 +0700

    Thêm 6 bảng: TK,PLTK,KH,LOAIKH,CTTT,DV+mã hóa pass

---
+++ three layer/BUS/BUS_KHACHHANG.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    public class BUS_KHACHHANG
+    {
+        DAL_KHACHHANG khachHang = new DAL_KHACHHANG();
+        public DataTable getTaiKhoan()
+        {
+            return khachHang.getTaiKhoan();
+        }
+        public bool ThemKhachHang(DTO_KHACHHANG kh)
+        {
+            return khachHang.ThemKhachHang(kh);
+        }
+
+        public bool SuaKhachHang(DTO_KHACHHANG kh)
+        {
+            return khachHang.SuaKhachHang(kh);
+        }
+
+        public bool XoaKhachHang(string makh)
+        {
+            return khachHang.XoaKhachHang(makh);
+        }
+
+        public bool TimKiemKhachHang(string s)
+        {
+            return khachHang.TimKiemKhachHang(s);
+        }
+    }
+}
---
+++ three layer/DAL/DAL_PHANLOAITK.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+namespace DAL
+{
+    public class DAL_PHANLOAITK:KetNoi
+    {
+        public DataTable getPhanLoaiTK()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM PHANLOAITAIKHOAN", connection);
+            DataTable dtPHANLOAITK = new DataTable();
+            da.Fill(dtPHANLOAITK);
+            return dtPHANLOAITK;
+        }
+        public bool ThemPhanLoaiTK(DTO_PHANLOAITK pltk)
+        {
+            connection.Open();
+            string sql = string.Format("NSERT INTO PHANLOAITAIKHOAN(TENLOAITAIKHOAN,QUYENHAN) VALUES ('{0}', '{1}')", pltk.TENLOAITK,pltk.QUYENHAN);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;
+            connection.Close();
+        }
+
+        public bool SuaPhanLoaiTK(DTO_PHANLOAITK pltk)
+        {
+            connection.Open();
+            string sql = string.Format("UPDATE PHANLOAITAIKHOAN SET TENLOAITAIKHOAN='{0}, QUYENHAN='{1}'",pltk.TENLOAITK,pltk.QUYENHAN);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;
+            connection.Close();
+        }
+
+        public bool XoaPhanLoaiTK(int maltk)
+        {
+            connection.Open();
+            string sql = string.Format("DELETE FROM PHANLOAITAIKHOAN WHERE MALOAITK = '{0}')", maltk);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;
+            connection.Close();
+        }
+    }
+}
