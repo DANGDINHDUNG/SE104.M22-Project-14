34b4d6a6a44ee55cb432c6c0e8091bb5e81623ca
Vinh <95330636+UIT20520862@users.noreply.github.com>
2022-05-12 23:25:03 +0700

    Add files via upload

---
+++ UI/QuanLyKhachSan/MVVM/View/TempView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyKhachSan.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for Temp.xaml
+    /// </summary>
+    public partial class TempView : Window
+    {
+        public TempView()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+    }
+}
---
+++ UI/QuanLyKhachSan/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using QuanLyKhachSan.Core;
+
+namespace QuanLyKhachSan.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand RoomsViewCommand { get; set; }
+        public RelayCommand RoomRentalViewCommand { get; set; }
+        public RelayCommand PaymentViewCommand { get; set; }
+        public RelayCommand ReportationViewCommand { get; set; }
+        public RelayCommand ServicesViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        public RoomsViewModel RoomsVM { get; set; }
+        public RoomRentalViewModel RoomRentalVM { get; set; }
+        public PaymentViewModel PaymentVM { get; set; }
+        public ReportationViewModel ReportationVM { get; set; }
+        public ServicesViewModel ServicesVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            RoomsVM = new RoomsViewModel();
+            RoomRentalVM = new RoomRentalViewModel();
+            PaymentVM = new PaymentViewModel();
+            ReportationVM = new ReportationViewModel();
+            ServicesVM = new ServicesViewModel();
+            SettingsVM = new SettingsViewModel();
+
+            CurrentView = RoomsVM;
+
+            RoomsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RoomsVM;
+            });
+
+            RoomRentalViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RoomRentalVM;
+            });
+
+            PaymentViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PaymentVM;
+            });
+
+            ReportationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ReportationVM;
+            });
+
+            ServicesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ServicesVM;
+            });
+
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+
+        }
+    }
+}
