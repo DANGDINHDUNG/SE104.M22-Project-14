9080c2827a1e66c84d4fbfacd061e36c579c924c
NGUYENGIABAO <92800277+ngb0511@users.noreply.github.com>
2022-05-31 19:24:03 +0700

    Add files via upload

---
+++ new update 31-5/DAL/DAL_LOAIPHONG.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using DTO;
+
+namespace DAL
+{
+    public class DAL_LOAIPHONG : KetNoi
+    {
+        public DataTable getDSLoaiPhong()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM LOAIPHONG", connection);
+            DataTable dtLoaiPhong = new DataTable();
+            da.Fill(dtLoaiPhong);
+            return dtLoaiPhong;
+        }
+
+        public bool ThemLoaiPhong(DTO_LOAIPHONG loaiPhong)
+        {
+            CheckConnection();
+            string sql = string.Format("INSERT INTO LOAIPHONG(MALP, TENLOAIPHONG, DONGIA) VALUES ('{0}', N'{1}', '{2}')", loaiPhong._MALP, loaiPhong._TENLOAIPHONG, loaiPhong._DONGIA);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;
+            connection.Close();
+        }
+
+        public bool SuaLoaiPhong(DTO_LOAIPHONG loaiPhong)
+        {
+            CheckConnection();
+            string sql = string.Format("UPDATE LOAIPHONG SET TENLOAIPHONG = N'{0}', DONGIA = '{1}' WHERE MALP = '{2}'", loaiPhong._TENLOAIPHONG, loaiPhong._DONGIA, loaiPhong._MALP);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;
+            connection.Close();
+        }
+
+        public bool XoaLoaiPhong(string maLP)
+        {
+            CheckConnection();
+            string sql = string.Format("DELETE FROM LOAIPHONG WHERE MALP = '{0}'", maLP);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;
+            connection.Close();
+        }
+
+        public string TimKiemTheoTenLoaiPhong(string tenLP)
+        {
+            string maLP = string.Empty;
+            CheckConnection();
+            string sql = string.Format("SELECT * FROM LOAIPHONG WHERE TENLOAIPHONG = N'{0}'", tenLP);
+
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            while (sdr.Read())
+            {
+                maLP = sdr["MALP"].ToString();
+            }
+            connection.Close();
+            return maLP;
+        }
+
+        public List<string> TongHopMaLoaiPhong()
+        {
+            List<string> listMaLoaiPhong = new List<string>();
+            CheckConnection();
+            string sql = string.Format("SELECT MALP FROM LOAIPHONG");
+
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            while (sdr.Read())
+            {
+                listMaLoaiPhong.Add(sdr[0].ToString());
+            }
+            connection.Close();
+            return listMaLoaiPhong;
+        }
+    }
+}
---
+++ new update 31-5/SubView/SettingRoomTypesView.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DTO;
+using BUS;
+using System.Data;
+
+namespace QuanLyKhachSan.MVVM.View.SubView
+{
+    /// <summary>
+    /// Interaction logic for SettingRoomTypesView.xaml
+    /// </summary>
+    public partial class SettingRoomTypesView : UserControl
+    {
+        public BUS_LOAIPHONG busLoaiPhong = new BUS_LOAIPHONG();
+
+        public SettingRoomTypesView()
+        {
+            InitializeComponent();
+            DataGridLoad();
+        }
+
+        private void AddButton_UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DTO_LOAIPHONG loaiPhong = new DTO_LOAIPHONG(roomTypeCodeTextBox.Text, roomTypeNameTextBox.Text, Decimal.Parse(priceTextBox.Text));
+
+            if (CheckExists())
+            {
+                busLoaiPhong.SuaLoaiPhong(loaiPhong);
+            }
+            else
+            {
+                busLoaiPhong.ThemLoaiPhong(loaiPhong);
+            }
+            DataGridLoad();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTextBoxes();
+            DataGridLoad();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            busLoaiPhong.XoaLoaiPhong(roomTypeCodeTextBox.Text);
+            DataGridLoad();
+        }
+
+        private void RoomTypeListDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (roomTypeListDataGrid.SelectedItems.Count == 0) return;
+            DataRowView row = roomTypeListDataGrid.SelectedItem as DataRowView;
+            if (row == null)
+            {
+                ClearTextBoxes();
+                return;
+            }
+            roomTypeCodeTextBox.Text = row[0].ToString();
+            roomTypeNameTextBox.Text = row[1].ToString();
+            priceTextBox.Text = row[2].ToString();
+        }
+
+        public void DataGridLoad()
+        {
+            roomTypeListDataGrid.DataContext = busLoaiPhong.getDSLoaiPhong();
+        }
+
+        public void ClearTextBoxes()
+        {
+            roomTypeCodeTextBox.Text = roomTypeNameTextBox.Text = priceTextBox.Text = string.Empty;
+        }
+
+        private bool CheckExists()
+        {
+            for (int i = 0; i < roomTypeListDataGrid.Items.Count - 1; i++)
+            {
+                DataRowView dataRowView = roomTypeListDataGrid.Items[i] as DataRowView;
+                if (dataRowView[1].ToString() == roomTypeNameTextBox.Text || dataRowView[0].ToString() == roomTypeCodeTextBox.Text)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+}
